ca5cdd3e66725520fcd3b68e404d8de5f9229859
Anvita67 <118798794+Anvita67@users.noreply.github.com>
2023-10-04 11:27:26 +0530

    Add files via upload

---
+++ springboot-thymeleaf-hello-world-example/src/main/java/net/javaguidesspringboot/SpringbootThymeleafHelloWorldExampleApplication.java
@@ -0,0 +1,13 @@
+package net.javaguidesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafHelloWorldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafHelloWorldExampleApplication.class, args);
+	}
+
+}
